ab8cf52738637e3358fabd69ca9e819603e52b51
Hylke Witjens <hylke@pti.nl>
2016-10-27 15:35:03 +0200

    I've learned some new asserts

--- tests/unit/WsdlGeneratorTest.php
+++ tests/unit/WsdlGeneratorTest.php
@@ -15,11 +15,11 @@ class WsdlGeneratorTest extends Test
 		$wsdl = $wsdlGenerator->generateWsdl(get_class($controller), 'http://test-url/');
 
 		$xml = simplexml_load_string($wsdl);
-		$this->assertTrue($xml instanceOf \SimpleXMLElement);
-		$this->assertTrue((string) $xml->getName() === 'definitions');
+		$this->assertInstanceOf(\SimpleXMLElement::class, $xml);
+		$this->assertSame((string) $xml->getName(), 'definitions');
 
 		$operation = $xml->xpath('//wsdl:operation[@name="getHello"]');
-		$this->assertTrue($operation[0] instanceof \SimpleXMLElement);
+		$this->assertInstanceOf(\SimpleXMLElement::class, $operation[0]);
 
 		$address = $xml->xpath('//soap:address');
 		$location = (string) $address[0]->attributes()->location;
