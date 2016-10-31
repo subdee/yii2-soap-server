704b093131e2a7cf2864af710edfdb8a2140623e
Hylke Witjens <hylke@pti.nl>
2016-10-31 13:09:27 +0100

    RequiredType was missing

---
+++ Validators/RequiredType.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types = 1);
+
+
+namespace subdee\soapserver\Validators;
+
+/**
+ * Required has no meaning for us, but we must implement it
+ * @author Hylke Witjens
+ * @version 20163110
+ * @package subdee\soapserver\Validators
+ */
+class RequiredType extends TrimType
+{
+
+}
