d7a60bcf9284c3c7ec4f98622484519cdc06647b
Hylke Witjens <hylke@pti.nl>
2016-11-28 16:25:10 +0100

    'on' always seems to contain only a string, not an array

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,4 +1,8 @@
 <?php
+error_reporting(-1);
+define('YII_ENABLE_ERROR_HANDLER',false);
+define('YII_DEBUG',true);
+
 // This is global bootstrap for autoloading 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
