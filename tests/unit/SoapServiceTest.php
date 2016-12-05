44919298180b4cb4ba74e98f984034a05c46efab
Hylke Witjens <hylke@pti.nl>
2016-12-05 11:37:06 +0100

    Added support for 'features' in the creating of the soapserver

--- tests/unit/SoapServiceTest.php
+++ tests/unit/SoapServiceTest.php
@@ -1,11 +1,14 @@
 <?php
-
 namespace subdee\soapserver\tests;
 
 use Codeception\TestCase\Test;
 use subdee\soapserver\tests\Controllers\SoapController;
 use subdee\soapserver\SoapService;
 
+/**
+ * @description Test for SoapService class
+ * @package subdee\soapserver\tests
+ */
 class SoapServiceTest extends Test
 {
     public function testGenerateWsdl()
@@ -24,6 +27,25 @@ class SoapServiceTest extends Test
 		$address = $xml->xpath('//soap:address');
 		$location = (string) $address[0]->attributes()->location;
 		$this->assertEquals('http://test-url/', $location);
+    }
+
+    /**
+     * We want to know for sure our new feature for ehm. features works
+     */
+    public function testFeatures()
+    {
+        $controller = new SoapController();
+
+        $soapService = new SoapService($controller, 'http://wsdl-url','http://test-url');
+        $soapService->features = [SOAP_SINGLE_ELEMENT_ARRAYS];
+
+        $reflection = new \ReflectionClass($soapService);
+
+        $method = $reflection->getMethod('getOptions');
+        $method->setAccessible(true);
+        $result = $method->invoke($soapService);
+
+        $this->assertEquals(1,$result['features'][0]);
     }
 
 }
